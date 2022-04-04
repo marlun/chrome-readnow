713f0829010cc777069b348652b819be99ba853e
Martin Lundberg <martin.lundberg@gmail.com>
2022-04-04 14:12:02 +0200

    Add ability to toggle between different pairs

--- readnow.js
+++ readnow.js
@@ -1,104 +1,166 @@
-'use strict'
+'use strict';
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'make-readable') {
-    makeReadable()
+    makeReadable();
   }
-})
+});
 
-function makeReadable () {
-  styleTarget(findWrapper())
+let variation = 0;
 
-  function findWrapper () {
-    const targetClasses = 'article,body,content,entry,hentry,main,page,post,text,blog,story,wrapper'
+function makeReadable() {
+  const variations = [
+    {
+      font: 'Open Sans SemiCondensed',
+      monoFont: 'Ubuntu Mono',
+      fontSize: '20px',
+      monoFontSize: '20px',
+    },
+    {
+      font: 'Merriweather Light',
+      monoFont: 'Ubuntu Mono',
+      fontSize: '20px',
+      monoFontSize: '20px',
+    },
+    {
+      font: 'Open Sans SemiCondensed',
+      monoFont: 'Ubuntu Mono',
+      fontSize: null,
+      monoFontSize: null,
+    },
+    {
+      font: 'Merriweather Light',
+      monoFont: 'Ubuntu Mono',
+      fontSize: null,
+      monoFontSize: null,
+    },
+  ];
 
-    let mostParagraphs = 0
-    let target = null
+  // Get current variation and increment to prepare the next
+  const { font, monoFont, fontSize, monoFontSize } =
+    variations[variation++ % variations.length];
+
+  styleTarget(findWrapper());
+
+  function findWrapper() {
+    const targetClasses =
+      'article,body,content,entry,hentry,main,page,post,text,blog,story,wrapper';
+
+    let mostParagraphs = 0;
+    let target = null;
 
     // Get the classes to search for
-    let selectors = targetClasses.split(',')
+    let selectors = targetClasses.split(',');
 
     // Create the selector strings
     selectors = selectors.map(function (item) {
-      return 'div[class*="' + item + '"],div[id*="' + item + '"],section[class*="' + item + '"]'
-    })
+      return (
+        'div[class*="' +
+        item +
+        '"],div[id*="' +
+        item +
+        '"],section[class*="' +
+        item +
+        '"]'
+      );
+    });
 
-    // Add the article element
-    selectors.push('article')
+    // Add other tags which is normally used for content
+    selectors.push('article');
+    selectors.push('main');
 
-    const elements = document.querySelectorAll(selectors.join(','))
+    const elements = document.querySelectorAll(selectors.join(','));
 
-    target = elements[0]
+    target = elements[0];
 
-    // for (let el of elements) {
     for (var i = 0; i < elements.length; i++) {
-      const el = elements[i]
-      const count = el.querySelectorAll('p').length
+      const el = elements[i];
+      const count = el.querySelectorAll('p').length;
       if (count > mostParagraphs) {
-        mostParagraphs = count
-        target = el
+        mostParagraphs = count;
+        target = el;
       }
     }
 
-    if (target == null) { target = document.body }
+    if (target == null) {
+      console.log('Could not find target, using body.');
+      target = document.body;
+    }
 
-    return target
+    return target;
   }
 
-  function styleTarget (target, prefs) {
-    let styles, elements
+  function styleTarget(target, prefs) {
+    let styles, elements;
 
+    const fontSizeRule = fontSize
+      ? `,font-size:${fontSize}`
+      : ',font-size:null';
+    const monoFontSizeRule = monoFontSize
+      ? `,font-size:${monoFontSize}`
+      : ',font-size:null';
     // Add styles to the target itself to fix some sites
     // that doesn't use paragraph elements
-    styles = parseStyles("font-family:'Fern Micro Testing',webkitFontSmoothing:subpixel-antialiased")
-    styleElements([target], styles)
+    styles = parseStyles(
+      `font-family:'${font}',line-height:1.6em,${fontSizeRule}`
+    );
+    styleElements([target], styles);
 
-    elements = target.querySelectorAll('p,p strong,p em')
-    styles = parseStyles("font-size:20px,font-family:'Fern Micro Testing',line-height:1.6em,letter-spacing:-0.02em,webkitFontSmoothing:subpixel-antialiased")
-    styleElements(elements, styles)
+    //"font-family:'Merriweather Light',line-height:1.6em,letter-spacing:-0.02em,"
+    elements = target.querySelectorAll('p,p strong,p em');
+    styles = parseStyles(
+      `font-family:'${font}',line-height:1.6em,${fontSizeRule}`
+    );
+    styleElements(elements, styles);
 
-    elements = target.querySelectorAll('li')
-    styles = parseStyles("font-size:20px,font-family:'Fern Micro Testing',line-height:1.6em,letter-spacing:-0.02em,webkitFontSmoothing:subpixel-antialiased")
-    styleElements(elements, styles)
+    elements = target.querySelectorAll('li');
+    styles = parseStyles(
+      `font-family:'${font}',line-height:1.6em,${fontSizeRule}`
+    );
+    styleElements(elements, styles);
 
-    elements = target.querySelectorAll('pre,code,.code')
-    styles = parseStyles("font-size:18px,font-family:'Fantasque Sans Mono',webkitFontSmoothing:subpixel-antialiased")
-    styleElements(elements, styles)
+    elements = target.querySelectorAll('pre,code,.code');
+    styles = parseStyles(
+      `font-family:'${monoFont}',fontVariantLigatures:none${monoFontSizeRule}`
+    );
+    styleElements(elements, styles);
 
-    elements = target.querySelectorAll('h1,h2,h3,h4')
-    styles = parseStyles("font-weight:700,font-family:'Fern Micro Testing',webkitFontSmoothing:subpixel-antialiased")
-    styleElements(elements, styles)
+    elements = target.querySelectorAll('h1,h2,h3,h4');
+    styles = parseStyles(`font-weight:700,font-family:'${font}',`);
+    styleElements(elements, styles);
 
-    elements = target.querySelectorAll('blockquote,blockquote p')
-    styles = parseStyles("font-size:20px,font-family:'Fern Micro Testing',line-height:1.6em,letter-spacing:-0.02em,webkitFontSmoothing:subpixel-antialiased")
-    styleElements(elements, styles)
+    elements = target.querySelectorAll('blockquote,blockquote p');
+    styles = parseStyles(
+      `font-family:'${font}',line-height:1.6em,${fontSizeRule}`
+    );
+    styleElements(elements, styles);
 
     // elements = target.querySelectorAll('p strong')
     // styles = parseStyles('font-weight:700')
     // styleElements(elements, styles)
   }
 
-  function parseStyles (prefString) {
-    const prefs = []
-    const pairs = prefString.split(',')
-    // for (let pair of pairs) {
+  function parseStyles(prefString) {
+    const prefs = [];
+    const pairs = prefString.split(',');
     for (var i = 0; i < pairs.length; i++) {
-      const pair = pairs[i]
-      const style = pair.split(':')
-      prefs.push({ name: style[0], value: style[1] })
+      const pair = pairs[i];
+      const style = pair.split(':');
+      prefs.push({ name: style[0], value: style[1] });
     }
-    return prefs
+    return prefs;
   }
 
-  function styleElements (elements, styles) {
-    // for (let el of elements) {
+  function styleElements(elements, styles) {
     for (var i = 0; i < elements.length; i++) {
-      const el = elements[i]
-      // for (let {name, value} of styles) {
-      // for (let style of styles) {
+      const el = elements[i];
       for (var n = 0; n < styles.length; n++) {
-        const style = styles[n]
-        el.style[style.name] = style.value
+        const style = styles[n];
+        if (style.value === 'null') {
+          el.style.removeProperty(style.name);
+        } else {
+          el.style[style.name] = style.value;
+        }
       }
     }
   }
